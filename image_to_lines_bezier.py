78a77b657cb3d451aa5f49c109bb769d5c4cd696
Ed Lewis <ed.lewis@gmail.com>
2018-03-21 14:23:53 -0400

    fix bezier curves

--- image_to_lines_bezier.py
+++ image_to_lines_bezier.py
@@ -4,7 +4,7 @@ import svgwrite
 import math
 from functools import reduce
 
-basewidth = 50
+basewidth = 40
 img = Image.open('california.png').convert('RGB')
 wpercent = (basewidth/float(img.size[0]))
 hsize = int((float(img.size[1])*float(wpercent)))
@@ -18,25 +18,31 @@ cols = len(im[0])
 
 def average_of_colors(rgb, max_height):
     average = reduce( (lambda x, y: int(x) + int(y)), rgb ) / len(rgb)
-    return average * max_height / 255
+    return int(average * max_height / 255)
 
 def make_line(row_array, row_number, dwg, cell_size, max_height):
     h_base = (row_number + 1) * cell_size
     points = len(row_array)
-    point_array = [[cell_size,h_base + max_height]]
-    for point in range(points):
-        point_array.append([(point + 1) * cell_size, h_base + average_of_colors(row_array[point], max_height)])
-    point_array.append([(points + 2) * cell_size,h_base + max_height])
-    dwg.add(dwg.polyline(point_array, fill="white", stroke="black", stroke_width=5))
+    line_max = h_base + max_height
+    point_string = "M " + str(cell_size) + ' ' + str(h_base + average_of_colors(row_array[1], max_height))
+    for point in range(1,points):
+        control_x = str((point + 1.5) * cell_size)
+        new_x = str((point + 2) * cell_size)
+        new_y = h_base + average_of_colors(row_array[point], max_height)
+        old_y = h_base + average_of_colors(row_array[point - 1], max_height)
+        if new_y > old_y:
+            point_string += ' S ' + control_x + ' ' + str(old_y) +  ' ' + new_x + ' ' + str(new_y)
+        else:
+            point_string += ' S ' + control_x + ' ' + str(new_y) +  ' ' + new_x + ' ' + str(new_y)
+    dwg.add(dwg.path(d=point_string, fill="white", stroke="black", stroke_width=5))
 
 
 def to_svg(cell_size = 30, max_height = 30):
 
     dwg = svgwrite.Drawing('./export/image_lines.svg')
-    dwg.add(dwg.path(d="m 0 0 t 800 70 t 500 200", fill="#ffff00", stroke='black'))
     for row in range(rows):
         make_line(im[row], row, dwg, cell_size, max_height)
 
     dwg.save()
 
-to_svg(30, 120)
+to_svg(20, 50)
